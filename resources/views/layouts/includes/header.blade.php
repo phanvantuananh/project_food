24dd3674fb61a95c6c4a5af7aab060d4341d8184
vnext-anhpvt <anhpvt@vnext.com.vn>
2021-11-08 16:21:22 +0700

    fix: Edit file scss user homepage

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -2,23 +2,23 @@
     <div class="">
         <div class="row">
             <div class="col-lg-12 col-sm-12 text-center">
-                <div class="logo">
+                <div class="menu__top__logo">
                     <a class="logo" href="" >Wow Food</a>
                 </div>
-                <div class="icon-toggle">
+                <div class="menu__top__icon__toggle">
                     <i class="fa fa-bars" aria-hidden="true"></i>
                 </div>
-                <nav class="main-menu">
+                <nav class="menu__top__main__menu">
                     <ul>
                         <li><a href="#" style="color: #F28123;">Home</a></li>
                         <li><a href="">News</a></li>
                         <li><a href="">Contact</a></li>
                         <li><a href="shop">Shop</a></li>
                         <li>
-                            <div class="menu__top__icons">
+                            <div class="menu__top__main__menu__icons">
                                 <a href=""><i class="fa fa-user-plus" aria-hidden="true"></i></a>
-                                <a class="shopping-cart" href=""><i class="fas fa-shopping-cart"></i></a>
-                                <a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+                                <a class="menu__top__main__menu__icons__shopping__cart" href=""><i class="fas fa-shopping-cart"></i></a>
+                                <a class="menu__top__main__menu__icons__heart" href=""><i class="fa fa-heart" aria-hidden="true"></i></a>
                             </div>
                         </li>
                     </ul>
